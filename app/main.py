42eece5287f2a59d14128d0978fb6ab2e80aa62d
kaelo-ai <kaelomotaung.ai@gmail.com>
2024-06-21 14:00:58 +0200

    Refactoring

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
 )
 
 # Trailer synopsis, plot and title created using AI
-trailers = open("../synopses.json", "r")
+trailers = open("synopses.json", "r")
 trailers = json.load(trailers)
 
 @app.get("/")
